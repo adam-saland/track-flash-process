094d4fea5cbe63ed1086da4c757aeac732ed755e
adam-saland <adam.saland@openfin.co>
2020-12-10 17:51:16 -0500

    launch and wait for argument

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,13 @@
-//event listeners.
+
 document.addEventListener('DOMContentLoaded', () => {
-    const ofVersion = document.querySelector("#of-version")
-    ofVersion.innerHTML = fin.desktop.getVersion();
-    const appAsset = {
-        src: "http://localhost:5555/find-offenders.zip",
-        version: "0.0.1",
-        alias: "find-offenders",
-        target: "find-offenders/FindOffenders.exe",
-        mandatory: true
-    }
-    function launchAsset(appAssetInfo) {
-        fin.desktop.System.launchExternalProcess({
-            alias: appAssetInfo.alias,
-            arguments: '800',
-            listener: function (e) {
-                console.log(`the exit code ${e.exitCode}`);
-            }
-        }, () => {
-            console.log('asset launched');
-        }, (reason, err) => {
-            console.log(reason, err);
-        });
-    }
+    const app = fin.Application.getCurrentSync();
+    fin.System.launchExternalProcess({
+        alias: 'find-offenders',
+        arguments: '800', //
+        listener: (code) => { if (code === 1) { app.restart() } },
+        target: 'FindOffenders.exe'
+    })
+    .then((data) => console.log('successfully launched FinderOffenders.exe: ', data))
+    .catch(console.error)
 
-    fin.desktop.System.getAppAssetInfo({alias: appAsset.alias}, appAssetInfo => {
-        console.log(`we already have the asset installed, let's just launch it.`);
-        launchAsset(appAssetInfo);
-    }, (r, err) => {
-
-        //Let's assume the asset is not available
-        console.log(`asset not found, let's download.`);
-        fin.desktop.System.downloadAsset(appAsset, progress => {
-
-            //Print progress as we download the asset.
-            const downloadedPercent = Math.floor((progress.downloadedBytes / progress.totalBytes) * 100);
-            console.log(`Downloaded ${downloadedPercent}%`);
-        }, () => {
-
-            //asset download complete, launch
-            launchAsset(appAsset);
-        }, (reason, error) => {
-
-            //Failed the download.
-            console.log(reason, error);
-        });
-
-    });
 });
